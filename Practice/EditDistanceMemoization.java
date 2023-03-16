925b6345d09b2445a208035fde3c4718683ac850
Muhammad Jaffar Abbas <63652832+Jaffar17@users.noreply.github.com>
2023-03-16 15:49:37 +0500

    Create EditDistanceMemoization.java
    Edit Distance - LeetCode - Hard
    
    Problem Link: https://leetcode.com/problems/edit-distance/description/

---
+++ Practice/EditDistanceMemoization.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()][word2.length()];
+        int i = word1.length()-1;
+        int j = word2.length()-1;
+        for(int n=0; n<=i; n++){
+            Arrays.fill(dp[n], -1);
+        }
+        return editDistance(i, j, word1, word2, dp);
+    }
+    public int editDistance(int i, int j, String word1, String word2, int[][] dp){
+        if(i<0){
+            return j+1;
+        }
+        if (j<0){
+            return i+1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(word1.charAt(i) == word2.charAt(j)){
+            return 0+editDistance(i-1, j-1, word1, word2, dp);
+        }
+        dp[i][j] = Math.min(1 + editDistance(i, j-1, word1, word2, dp), // insert
+        Math.min(1 + editDistance(i-1, j, word1, word2, dp), // delete
+        1+ editDistance(i-1, j-1, word1, word2, dp))); // replace
+
+        return dp[i][j];
+    }
+}
