2550d6f5513ab0f745a8969854cf1e0904f7a0d7
Muhammad Jaffar Abbas <63652832+Jaffar17@users.noreply.github.com>
2023-03-18 22:31:27 +0500

    Create LongestIncreasingSubsequence.java
    Longest Increasing Subsequence - LeetCode
    
    Problem Link: https://leetcode.com/problems/longest-increasing-subsequence/submissions/917613690/

---
+++ Practice/LongestIncreasingSubsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[][] dp = new int[nums.length][nums.length+1];
+        for(int i=0; i<nums.length; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        int prev_ind = -1;
+        int ind = 0;
+        return recLIS(ind, prev_ind, nums, dp);
+    }
+
+    public int recLIS(int ind, int prev_ind, int[] nums, int[][] dp){
+        if(ind == nums.length){
+            return 0;
+        }
+
+        if(dp[ind][prev_ind+1] !=-1){
+            return dp[ind][prev_ind+1];
+        }
+
+        int notTake = 0 + recLIS(ind+1, prev_ind, nums, dp);
+        int take = 0;
+        if(prev_ind == -1 || nums[ind]>nums[prev_ind]){
+            take = 1 + recLIS(ind+1, ind, nums, dp);
+        }
+
+        dp[ind][prev_ind+1] = Math.max(take, notTake);
+        return dp[ind][prev_ind+1];
+    }
+}
