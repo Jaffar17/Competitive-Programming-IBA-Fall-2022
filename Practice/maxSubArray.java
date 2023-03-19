394a11d48acb53a30d6960cf5fc179f8948e7c14
Muhammad Jaffar Abbas <63652832+Jaffar17@users.noreply.github.com>
2023-03-19 14:37:20 +0500

    Update maxSubArray.java

--- Practice/maxSubArray.java
+++ Practice/maxSubArray.java
@@ -14,3 +14,29 @@ class Solution {
         return maxSum;
     }
 }
+
+
+// Optimal Approach:
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	
+	public static long maxSubarraySum(int[] arr, int n) {
+		// write your code here
+		long local_max = 0;
+		long global_max = Long.MIN_VALUE;
+
+		for(int i=0; i<n; i++){
+			local_max = Math.max(arr[i], arr[i]+local_max);
+			if(local_max<0){
+				local_max = 0;
+			}
+			if(local_max>global_max){
+				global_max = local_max;
+			}
+		}
+		return global_max;
+	}
+
+}
