7bae55a0c3a33b91ad3a9f17b72fc73d351b905e
Muhammad Jaffar Abbas <63652832+Jaffar17@users.noreply.github.com>
2023-03-20 00:58:35 +0500

    Update LongestIncreasingSubsequence.java

--- Practice/LongestIncreasingSubsequence.java
+++ Practice/LongestIncreasingSubsequence.java
@@ -28,3 +28,22 @@ class Solution {
         return dp[ind][prev_ind+1];
     }
 }
+
+// Tabulation Approach
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[][] dp = new int[nums.length+1][nums.length+1];
+
+        for(int ind=nums.length-1; ind>=0; ind--){
+            for(int prev_ind = ind-1; prev_ind>=-1; prev_ind--){
+                int notTake = 0 + dp[ind+1][prev_ind+1];
+                int take = 0;
+                if(prev_ind == -1 || nums[ind]>nums[prev_ind]){
+                    take = 1 + dp[ind+1][ind+1];
+                    }
+                dp[ind][prev_ind+1] = Math.max(take, notTake);
+            }
+        }
+        return dp[0][0];
+    }
+}
